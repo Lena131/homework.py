515b08231078d81e6b52857bc53ccd103ed757f9
Mandik Elena Anatolevna <113716827+Lena131@users.noreply.github.com>
2023-02-05 22:18:09 +0500

    Негафибоначчи

--- DZ3-05.py
+++ DZ3-05.py
@@ -7,10 +7,12 @@ if n < 0:
     n = n*-1
 f1 = f2 = 1
 ist1 = [f1, f2]
+
 for i in range(2, n):
     f1,f2 = f2, f1 + f2
     list1.append(f2)
 f1=f2=1
+
 for i in range(-n, 1):
     f1,f2 = f2, f1 - f2
     list1.insert(0, f2)
